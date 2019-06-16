cfcce0aab5e7185e45a496de1c0313e1d82cc59d
mchen <cmm95@outlook.com>
2019-06-16 13:21:39 -0700

    Update CameraData.py

--- CameraData.py
+++ CameraData.py
@@ -10,20 +10,12 @@ class CameraData:
             # Capture frame-by-frame
             ret, frame = cap.read()
             print(np.array(frame))
-            # Our operations on the frame come here
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            # Display the resulting frame
-            cv2.imshow('frame', gray)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
         # When everything done, release the capture
         cap.release()
         cv2.destroyAllWindows()
-
-
-
         pass
 
     def getDataArray(self):
