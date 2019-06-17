e22aab508b7f4c649e72d69d8a95c4ea46ff8bfe
mchen <cmm95@outlook.com>
2019-06-17 15:49:31 -0700

    update EchoServer .py

--- EchoServer.py
+++ EchoServer.py
@@ -3,10 +3,11 @@ from tensorpack.dataflow import *
 class EchoServer:
 
     def __init__(self):
-        ds0 = FakeData([[2,3]])
-        send_dataflow_zmq(ds0, 'tcp://10.0.0.201:8877')
-        
-        pass
+        while (True):
+            ds0 = FakeData([[2,3]])
+            send_dataflow_zmq(ds0, 'tcp://10.0.0.201:8877')
+
+
 
 
 
