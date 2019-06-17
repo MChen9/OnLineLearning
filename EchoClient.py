c24a70a2ff705b93b522ba038659a55c64ac1164
mchen <cmm95@outlook.com>
2019-06-17 15:17:41 -0700

    create EchoClient.py

---
+++ EchoClient.py
@@ -0,0 +1,13 @@
+from tensorpack.dataflow import *
+
+
+class EchoClient:
+
+    def __init__(self):
+        df = RemoteDataZMQ('tcp://10.0.0.120:8877')
+
+        pass
+
+
+if __name__ == '__main__':
+    echoClient = EchoClient()
--- EchoServer.py
+++ EchoServer.py
@@ -4,4 +4,17 @@ class EchoServer:
 
     def __init__(self):
         ds0 = FakeData([[2,3]])
-        send_dataflow_zmq(ds0, 'tcp://10.0.0.120:8877')
+        send_dataflow_zmq(ds0, 'tcp://10.0.0.201:8877')
+        
+        pass
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    echoServer = EchoServer()
